aeecb1353ab20207a7d7c6dcee8de2fe306955ac
Julian Toscani <julian.toscani@grapes.de>
2023-10-25 09:31:03 +0200

    feat: translate card on presentation layer

--- src/app/components/PlayCard.tsx
+++ src/app/components/PlayCard.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
-import { TranslatedCard } from "../cards/types";
+import { Card } from "../../game/types";
+import { german } from "../../game/translations";
 
-export const PlayCard: FC<{ card: TranslatedCard }> = ({ card }) => (
-  <li className="relative w-60 h-96 flex justify-center items-center p-4 border-white border rounded-md">
-    <span className="absolute top-4 left-4"> {card.color} </span>
-    <span className="absolute top-4 right-4"> {card.color} </span>
-    <span className="absolute bottom-4 left-4"> {card.color} </span>
-    <span className="absolute bottom-4 right-4"> {card.color} </span>
 
-    <div>
-      <span>{card.picture}</span> {': '}
-      <span>{card.points}</span>
-    </div>
-  </li>
-);
+  export const PlayCard: FC<{ card: Card }> = ({ card }) => {
+    const color = german.colors[card.color];
+    const picture = german.pictures[card.picture];
+    return (
+      <li className="relative w-60 h-96 flex justify-center items-center p-4 bg-white text-black border-black border rounded-md">
+        <span className="absolute top-4 left-4"> {color} <br /> <small>{picture}</small> </span>
+        <span className="absolute top-4 right-4"> {color} <br /> <small>{picture}</small> </span>
+        <span className="absolute bottom-4 left-4"> {color} <br /> <small>{picture}</small> </span>
+        <span className="absolute bottom-4 right-4"> {color} <br /> <small>{picture}</small> </span>
+
+        <div>
+          <span>{picture}</span> {": "}
+          <span>{card.points}</span>
+        </div>
+      </li>
+    );
+  };

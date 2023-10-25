1524c81edebaf3f5f616e79999c8cf774424a484
Julian Toscani <julian.toscani@grapes.de>
2023-10-25 09:32:44 +0200

    feat: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import { germanDeck } from "./cards";
+import { cards } from "../game/cards";
+import { PlayerHand } from "./components/PlayerHand";
 import { PlayCard } from "./components/PlayCard";
+import { dealHands } from "@/game/dealHands";
 
 export default function Home() {
+  const hands = dealHands()
+
   return (
     <main className="container mx-auto">
+      <PlayerHand cards={hands[0]} />
       <ul className="grid grid-cols-5 gap-4">
-        {germanDeck.map((card) => (
+        {cards.map((card) => (
           <PlayCard key={card.id} card={card} />
         ))}
       </ul>

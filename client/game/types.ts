f45a7899664d3bf0fcbf01a942e533948112aae2
Julian Toscani <julian_toscani@gmx.de>
2023-11-14 22:12:15 +0100

    feat: use pojos instead of enums (#26)
    Co-authored-by: Julian Toscani <julian.toscani@grapes.de>

--- client/game/types.ts
+++ client/game/types.ts
@@ -1,51 +1,54 @@
-import type React from "react";
+export const Color = {
+	Spade: 1,
+	Hearth: 2,
+	Club: 3,
+	Diamond: 4,
+} as const;
 
-export enum Color {
-  Spade = 1,
-  Hearth,
-  Club,
-  Diamond,
-}
+export const Picture = {
+	Ten: 1,
+	Jack: 2,
+	Queen: 3,
+	King: 4,
+	Ace: 5,
+} as const;
 
-export enum Picture {
-  Ten = 1,
-  Jack,
-  Queen,
-  King,
-  Ace,
-}
+type Value<T extends {}> = T[keyof T]
 
-export type CardId = `${1 | 2}${Color}${Picture}`
+type ColorV = Value<typeof Color>
+type PictureV = Value<typeof Picture>
+
+export type CardId = `${'1' | '2'}${ColorV}${PictureV}`;
 
 export interface Card {
-  picture: Picture;
-  color: Color;
-  points: number;
-  id: CardId;
-};
-
-export interface OrderedCard extends Card {
-  order: number,
-  trump: boolean
-} 
-
-export type TranslatedCard = {
-  picture: string;
-  color: string;
-  points: number;
-  id: CardId;
-};
-
-type ColorNames = Record<Color, string>;
-type PictureNames = Record<Picture, string>;
-
-export type DeckTranslation = {
-    colorNames: ColorNames;
-    pictureLetters: PictureNames;
-    pictureLabels: PictureNames;
+	picture: PictureV;
+	color: ColorV;
+	points: number;
+	id: CardId;
 }
 
+export interface OrderedCard extends Card {
+	order: number;
+	trump: boolean;
+}
+
+export type TranslatedCard = {
+	picture: string;
+	color: string;
+	points: number;
+	id: CardId;
+};
+
+type ColorNames = Record<ColorV, string>;
+type PictureNames = Record<PictureV, string>;
+
+export type DeckTranslation = {
+	colorNames: ColorNames;
+	pictureLetters: PictureNames;
+	pictureLabels: PictureNames;
+};
+
 export type CardOrderTempValue = { id: CardId; trump: boolean };
-export type GameOrderEntry = { trump: boolean, order: number }
-export type GameOrder = Record<CardId, GameOrderEntry>
-export type DealtHands<C = OrderedCard> = [Array<C>, Array<C>, Array<C>, Array<C>]
+export type GameOrderEntry = { trump: boolean; order: number };
+export type GameOrder = Record<CardId, GameOrderEntry>;
+export type DealtHands<C = OrderedCard> = [Array<C>, Array<C>, Array<C>, Array<C>];

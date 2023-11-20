95274f5c6205172973c709e9f7453676ecae1e49
Julian Toscani <julian.toscani@grapes.de>
2023-11-20 23:03:10 +0100

    feat: update game context

--- client/context/game.tsx
+++ client/context/game.tsx
@@ -1,46 +1,59 @@
-"use client";
+'use client';
 
-import {
-  FC,
-  PropsWithChildren,
-  createContext,
-  useEffect,
-  useState,
-} from "react";
-import { Card, GameOrder, OrderedCard } from "shared/types";
-import { OpponentState, opponents } from "../game/opponent";
+import { FC, PropsWithChildren, createContext, useEffect, useState } from 'react';
+import { opponents } from 'shared/constants';
+import { OpponentState, OrderedCard } from 'shared/types';
 
 type GameContext = {
-  canPlayCard: boolean;
-  hand: Array<OrderedCard>;
-  opponents: Array<OpponentState>;
-  table: Array<OrderedCard>;
-  playCard: (_card: OrderedCard) => void;
+	canPlayCard: boolean;
+	hand: Array<OrderedCard>;
+	opponents: Array<OpponentState>;
+	table: Array<OrderedCard>;
+	playCard: (_card: OrderedCard) => void;
 };
 
+const API_HOST = 'http://localhost:4000';
 export const GameContext = createContext<GameContext | null>(null);
 
-export const GameContextProvider: FC<PropsWithChildren<{hand: Array<Card>}>> = ({ children, hand }) => {
-  const [order, _setOrder] = useState<GameOrder>(defaultOrder);
-  const [orderedHand, setOrderedHand] = useState<GameContext["hand"]>([]);
-  const [table, setTable] = useState<Array<OrderedCard>>([]);
+export const GameContextProvider: FC<PropsWithChildren<{ gameId: string }>> = ({
+	children,
+	gameId,
+}) => {
+	const [orderedHand, setOrderedHand] = useState<GameContext['hand']>([]);
+	const [table, setTable] = useState<Array<OrderedCard>>([]);
 
-  const playCard = (card: OrderedCard) => {
-    setOrderedHand(orderedHand.filter((inHand) => inHand.id !== card.id));
-    setTable([...table, card]);
-  };
+	const playCard = (card: OrderedCard) => {
+		setOrderedHand(orderedHand.filter((inHand) => inHand.id !== card.id));
+		setTable([...table, card]);
+	};
 
-  useEffect(() => {
-    const orderedCards = applyOrder(hand, order);
-    sortHand(orderedCards);
-    setOrderedHand(orderedCards);
-  }, [setOrderedHand, order, hand]);
+	useEffect(() => {
+		const gameIdPromise = gameId
+			? Promise.resolve(gameId)
+			: fetch(`${API_HOST}/game/new`, {
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+			  }).then((response) => response.json());
 
-  return (
-    <GameContext.Provider
-      value={{ hand: orderedHand, table, canPlayCard: true, playCard, opponents }}
-    >
-      {children}
-    </GameContext.Provider>
-  );
+		gameIdPromise.then((id) => {
+			fetch(`${API_HOST}/game/${id}/id?player=1`, {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then((response) => response.json())
+				.then((hand) => setOrderedHand(hand))
+				.catch(console.error);
+		});
+	});
+
+	return (
+		<GameContext.Provider
+			value={{ hand: orderedHand, table, canPlayCard: true, playCard, opponents }}
+		>
+			{children}
+		</GameContext.Provider>
+	);
 };

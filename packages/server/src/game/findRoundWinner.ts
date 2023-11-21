56b10e93518364b17ea2b4b3b7b8e011dcbf26f0
Julian Toscani <julian.toscani@grapes.de>
2023-11-21 22:52:08 +0100

    refactor: put bigger functions in modules

---
+++ packages/server/src/game/findRoundWinner.ts
@@ -0,0 +1,17 @@
+import { Game } from 'shared';
+
+export const findRoundWinner = (round: Game['rounds'][number]): string => {
+	const FIRST_PLAYED_INDEX = 0;
+	const hasTrumpCards =
+		round[FIRST_PLAYED_INDEX].card.trump || round.some(({ card }) => card.trump);
+
+	const cardsToConsider = hasTrumpCards
+		? round
+		: round.filter(({ card }) => card.color === round[FIRST_PLAYED_INDEX].card.color);
+
+	const maxOrder = Math.max(...cardsToConsider.map(({ card }) => card.order));
+
+	// cardsToConsider is subset of round
+	// as order is included in the subset, it will also be included in the bigger set
+	return round.find(({ card }) => card.order === maxOrder)!.from;
+};

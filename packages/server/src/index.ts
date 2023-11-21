97a9ee2a6642b5a079a7316c063c0542c502512c
Julian Toscani <julian.toscani@grapes.de>
2023-11-21 20:58:57 +0100

    fix: dependency extravaganza

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,13 +4,14 @@ import { BadRequestError, NotFoundError, handleError } from './errors';
 import { createGame, playCard } from './game/game';
 import { randomUUID } from 'crypto';
 import { Game, isCardId } from 'shared/types';
+import { cors } from '@elysiajs/cors';
 
 const PORT = 4000;
 
 const app = new Elysia();
 
 const games: Map<string, Game> = new Map();
-
+app.use(cors());
 app.onRequest((request) => Logger.info(`${request.path} =>`));
 app.onError(handleError);
 
@@ -25,51 +26,56 @@ app.put(
 	},
 	{
 		body: t.Object({
-            player: t.Array(t.String())
-        }),
+			player: t.Array(t.String()),
+		}),
 	},
 );
 
-app.put('/game/:id/card', ({ body, params }) => {
-    const { id } = params;
-    const game = games.get(id)
+app.put(
+	'/game/:id/card',
+	({ body, params }) => {
+		const { id } = params;
+		const game = games.get(id);
 
-    if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`)
-    if (!isCardId(body.card)) throw new BadRequestError(`String ${body.card} is not a valid [card.id].`)
+		if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`);
+		if (!isCardId(body.card))
+			throw new BadRequestError(`String ${body.card} is not a valid [card.id].`);
 
-    playCard(game, body.player, body.card)
+		playCard(game, body.player, body.card);
 
-    return {
-        table: game.table,
-        hand: game.hands[body.player]
-    }
-},{
-    body: t.Object({
-        card: t.String(),
-        player: t.String()
-    })
-})
+		return {
+			table: game.table,
+			hand: game.hands[body.player],
+		};
+	},
+	{
+		body: t.Object({
+			card: t.String(),
+			player: t.String(),
+		}),
+	},
+);
 
 app.get('/game/:id/hand', ({ params, query }) => {
 	const { id } = params;
-    const { player } = query;
+	const { player } = query;
 
-    const game = games.get(id)
-    if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`)
+	const game = games.get(id);
+	if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`);
 
-    const hand = player ? game.hands[player] : null
-    if (!hand) throw new NotFoundError(`Player with [id] ${player} does not exist.`)
+	const hand = player ? game.hands[player] : null;
+	if (!hand) throw new NotFoundError(`Player with [id] ${player} does not exist.`);
 
-	return hand
+	return hand;
 });
 
 app.get('/game/:id/table', ({ params }) => {
 	const { id } = params;
 
-    const game = games.get(id)
-    if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`)
+	const game = games.get(id);
+	if (!game) throw new NotFoundError(`Game with [id] ${id} does not exist.`);
 
-	return game.table
+	return game.table;
 });
 
 app.listen(PORT);

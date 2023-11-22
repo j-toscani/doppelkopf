a5544fc811234dca706d22393a46e2d58ba5bf03
Julian Toscani <julian.toscani@grapes.de>
2023-11-22 10:02:13 +0100

    feat: add create card utility

--- packages/server/src/game/cards.ts
+++ packages/server/src/game/cards.ts
@@ -1,11 +1,11 @@
-import  { Card, Color, type PictureV, Picture, ColorV } from "shared/types";
+import { Card, Color, type PictureV, Picture, ColorV } from 'shared/types';
 
 const points: Record<PictureV, number> = {
-  [Picture.Jack]: 2,
-  [Picture.Queen]: 3,
-  [Picture.King]: 4,
-  [Picture.Ten]: 10,
-  [Picture.Ace]: 11,
+	[Picture.Jack]: 2,
+	[Picture.Queen]: 3,
+	[Picture.King]: 4,
+	[Picture.Ten]: 10,
+	[Picture.Ace]: 11,
 };
 
 const isColor = (v: ColorV | string): v is ColorV => !!Number(v);
@@ -14,12 +14,16 @@ const isPicture = (v: PictureV | string): v is PictureV => !!Number(v);
 const colors = Object.values(Color).filter(isColor);
 const pictures = Object.values(Picture).filter(isPicture);
 
-export const copies = ['1','2'] as const
-export const cards: Array<Card> = copies.flatMap((copy) => colors.flatMap((color) =>
-  pictures.map((picture) => ({
-    picture,
-    color,
-    points: points[picture],
-    id: `${copy}${color}${picture}`,
-  }))
-));
+export const copies = ['1', '2'] as const;
+type CopyV = (typeof copies)[number];
+
+export const createCard = (copy: CopyV, color: ColorV, picture: PictureV): Card => ({
+	picture,
+	color,
+	points: points[picture],
+	id: `${copy}${color}${picture}`,
+});
+
+export const cards: Array<Card> = copies.flatMap((copy) =>
+	colors.flatMap((color) => pictures.map((picture) => createCard(copy, color, picture))),
+);

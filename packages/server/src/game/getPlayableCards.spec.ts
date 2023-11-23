546c162404fcb7eb9790289f6db8b38f109e1a83
Julian Toscani <julian.toscani@grapes.de>
2023-11-23 22:55:05 +0100

    wip(getPlayableCards): define test cases

---
+++ packages/server/src/game/getPlayableCards.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "bun:test";
+
+describe("Get Playable Cards", () => {
+    it("Can play all trump cards if trump round", () => {})
+    it("Can play all fehl cards on fehl round if does has respective fehl in hand", () => {})
+    it("Can play all cards on fehl round if does not have respective fehl in hand", () => {})
+    it("Can play all cards on trump round if does not have respective fehl in hand", () => {})
+})

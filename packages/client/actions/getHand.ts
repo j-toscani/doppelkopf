b5c8af10b5e97a0c011a5c022f30522210c61f5f
Julian Toscani <julian_toscani@gmx.de>
2024-01-20 22:47:35 +0100

    feat: encapsulate actionsserver calls (#47)
    * feat: add fetch abstraction
    
    * refactor: use request abstraction
    
    * format: add underscore to unused variable
    
    ---------
    
    Co-authored-by: Julian Toscani <julian.toscani@grapes.de>

--- packages/client/actions/getHand.ts
+++ packages/client/actions/getHand.ts
@@ -1,17 +1,5 @@
-import { API_HOST } from '@/constants';
 import { OrderedCard } from 'shared';
+import { getHand as getHandRequest } from '@/requests';
 
 export const getHand = (gameId: string, playerId: string): Promise<Array<OrderedCard>> =>
-	fetch(`${API_HOST}/games/${gameId}/hand?player=${playerId}`, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	})
-		.then((response) => {
-			if (response.ok) {
-				return response.json();
-			}
-
-			throw new Error(response.status.toString());
-		})
-		.then(({ hand }) => hand);
+	getHandRequest(gameId, playerId).then(({ hand }) => hand);
---
+++ packages/client/utils/request.ts
@@ -0,0 +1,39 @@
+import { API_HOST } from '@/constants';
+import { URL } from 'url';
+
+type Method = 'GET' | 'PUT' | 'POST' | 'DELETE';
+type RequestOptions = {
+	input?: unknown;
+	method?: Method;
+	query?: Record<string, string>;
+	headers?: Record<string, string>;
+};
+
+const queryToString = (query: Record<string, string>) =>
+	Object.entries(query)
+		.map(([k, v]) => `${k}=${v}`)
+		.join('&');
+
+export const request = async <Result>(
+	path: string,
+	options: RequestOptions = {},
+): Promise<Result> => {
+	const { method = 'GET', input, query = {}, headers = {} } = options;
+	const search = queryToString(query);
+	const url = new URL(search.length ? `${path}?${search}` : path, API_HOST);
+
+	const response = await fetch(url, {
+		method,
+		headers: {
+			'Content-Type': 'application/json',
+			...headers,
+		},
+		body: input ? JSON.stringify(input) : null,
+	});
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	} else {
+		return response.json();
+	}
+};

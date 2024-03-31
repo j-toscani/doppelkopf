110e576979a00d89cf42e6c1e8ffa2d505d01bc6
Julian Toscani <julian_toscani@gmx.de>
2024-03-31 17:27:28 +0200

    feat: get number of cards in opponents hand (#56)
    * fix: use correct id in error message
    
    * fix: move util to where it´s used
    
    * feat: allow missing user
    
    * feat: add getOpponentCardCount
    
    * test: add getOpponentCardCount.ts
    
    ---------
    
    Co-authored-by: Julian Toscani <julian.toscani@grapes.de>

---
+++ packages/server/src/game/getOpponentCardCount.spec.ts
@@ -0,0 +1,69 @@
+import { beforeEach, describe, expect, it } from 'bun:test';
+import { ADD_ONE, Game, User } from 'shared';
+import { createGame } from '../db/createGame';
+import { getOpponentCardCount } from './getOpponentCardCount';
+import { FIRST_ARRAY_INDEX } from '../constants';
+
+const users: Array<User> = [{ name: 'John' }, { name: 'Ron' }, { name: 'Sophy' }, { name: 'Mary' }];
+
+let game: Game;
+
+const cases = [
+	{ player: 'John', left: 'Ron', right: 'Mary' },
+	{ player: 'Ron', left: 'Sophy', right: 'John' },
+	{ player: 'Sophy', left: 'Mary', right: 'Ron' },
+	{ player: 'Mary', left: 'John', right: 'Sophy' },
+];
+
+describe('Get Opponent Card Count', () => {
+	beforeEach(() => {
+		game = createGame({ seats: users });
+	});
+	it('Returns the number of cards in opponents hand + userId as an array', () => {
+		game.hands.forEach((hand) => hand.pop());
+
+		const EXPECTED_CARDS_IN_HAND = 9;
+		const result = getOpponentCardCount(game, 'John');
+
+		expect(result.every((r) => r.cardsInHand === EXPECTED_CARDS_IN_HAND)).toBe(true);
+	});
+	it('Retuns correct number of cards played if not all players played even amount of cards', () => {
+		game.hands.forEach((hand) => hand.pop());
+
+		const EXPECTED_CARDS_IN_HAND = 9;
+		const SECOND_PLAYER_INDEX = 1;
+
+		game.hands[SECOND_PLAYER_INDEX].pop();
+
+		const result = getOpponentCardCount(game, 'John');
+
+		expect(result.every((r) => r.cardsInHand === EXPECTED_CARDS_IN_HAND)).toBe(false);
+		expect(result[FIRST_ARRAY_INDEX].cardsInHand).toBe(EXPECTED_CARDS_IN_HAND - ADD_ONE);
+	});
+	cases.forEach(({ player, left, right }) => {
+		describe(player, () => {
+			it('First item in the array returned is the user sitting on the left from current user', () => {
+				game.hands.forEach((hand) => hand.pop());
+
+				const [first] = getOpponentCardCount(game, player);
+
+				expect(first.user).toBe(left);
+			});
+			it('Last item in the array returned is the user sitting on the right from current user', () => {
+				game.hands.forEach((hand) => hand.pop());
+
+				const [_, __, last] = getOpponentCardCount(game, player);
+
+				expect(last.user).toBe(right);
+			});
+		});
+	});
+	it('Returns `[]` if user is not in the game anymore', () => {
+		game.hands.forEach((hand) => hand.pop());
+		game.seats[FIRST_ARRAY_INDEX] = null;
+
+		const result = getOpponentCardCount(game, 'John');
+
+		expect(result).toHaveLength(FIRST_ARRAY_INDEX);
+	});
+});

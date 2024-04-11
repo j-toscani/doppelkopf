345bfbbd7459fc06efe2ac61098933ea1e541729
Julian Toscani <julian.toscani@grapes.de>
2024-04-11 23:14:48 +0200

    feat: create end ob game states

--- packages/server/src/game/getEndOfGameState.ts
+++ packages/server/src/game/getEndOfGameState.ts
@@ -1,9 +1,10 @@
-import { Color, Game, OrderedCard, Picture } from 'shared';
+import { Color, Game, OrderedCard, Picture, Table } from 'shared';
 import { NATURAL_ZERO } from '../constants';
+import { findRoundWinner } from './findRoundWinner';
 
 type GroupedCards = Record<string, Array<OrderedCard>>;
 
-const getGroupedCards = (game: Game) =>
+const recreateHands = (game: Game) =>
 	game.rounds.reduce<GroupedCards>((acc, curr) => {
 		curr.forEach(({ card, from }) =>
 			from in acc ? acc[from].push(card) : (acc[from] = [card]),
@@ -11,20 +12,41 @@ const getGroupedCards = (game: Game) =>
 		return acc;
 	}, {});
 
-const calculatePoints = (cards: Array<OrderedCard>) =>
-	cards.reduce<number>((points, card) => card.points + points, NATURAL_ZERO);
+const calculateTablePoints = (table: Table) =>
+	table.reduce<number>((points, { card }) => card.points + points, NATURAL_ZERO);
 
-const getEndOfGameState = (userId: string, cards: Array<OrderedCard>) => {
-	const points = calculatePoints(cards);
+const calculateGamePoints = (rounds: Array<Table>) =>
+	rounds.reduce<Record<string, number>>((acc, table) => {
+		const winner = findRoundWinner(table);
+		if (winner in acc) {
+			acc[winner] += calculateTablePoints(table);
+		} else {
+			acc[winner] = calculateTablePoints(table);
+		}
+		return acc;
+	}, {});
+
+const getParties = (groupedCards: GroupedCards) => Object.entries(groupedCards).reduce<{ re: Array<string>, fel: Array<string> }>(
+	(acc, [user, cards]) => {
+		const isRe = !!cards.find(
+			(card) => card.picture === Picture.Queen && card.color === Color.Spade,
+		);
+		acc[isRe ? 're' : 'fel'].push(user)
+		return acc;
+	},
+	{ re: [], fel: [] },
+);
+
+export const getEndOfGameStates = (game: Game) => {
+	const hands = recreateHands(game);
+	const parties = getParties(hands)
+	const points = calculateGamePoints(game.rounds)
+
 	return {
-		userId,
-		points,
-		cards,
-		isRe: !!cards.find((card) => card.color === Color.Club && card.picture === Picture.Queen),
-	};
+		points: {
+			re: parties.re.reduce((acc, user) => points[user] + acc, NATURAL_ZERO),
+			fel: parties.fel.reduce((acc, user) => points[user] + acc, NATURAL_ZERO)
+		},
+		parties
+	}
 };
-
-export const getEndOfGameStates = (game: Game) =>
-	Object.entries(getGroupedCards(game))
-		.map(([userId, cards]) => getEndOfGameState(userId, cards))
-		.sort(({ points: pointsA }, { points: pointsB }) => pointsA - pointsB);

5b8031530a725cb441fa38fcd8cd26dc56e2b920
Julian Toscani <julian.toscani@grapes.de>
2024-05-01 21:50:17 +0000

    chore: adjust env for devcontainer setup
    Signed-off-by: Julian Toscani <julian.toscani@grapes.de>

--- packages/server/src/environment.ts
+++ packages/server/src/environment.ts
@@ -2,10 +2,10 @@ const DEFAULT_PORT = 4000;
 
 const PORT = process.env.PORT ?? DEFAULT_PORT;
 const ORIGIN = process.env.DEVELOPMENT ? '*' : process.env.ORIGIN;
-const MONGODB_URL = process.env.MONGOBD_URL ?? 'mongodb://127.0.0.1:27016';
+const MONGODB_URL = process.env.MONGOBD_URL ?? 'mongodb://127.0.0.1:27017';
 const DBUSER_PASSWORD = process.env.MONGODB_PASSWORD || '123';
 const DBUSER_NAME = process.env.MONGODB_USERNAME ?? 'user';
-const MONGODB_DATABASENAME = process.env.MONGODB_DATABASE ?? 'doppelkopf';
+const MONGODB_DATABASENAME = process.env.MONGODB_DATABASE;
 
 export const environment = {
 	PORT,
